a14fda3ae3584e359aa69c1347e407cde8c58e63
Waradu <waradu@outlook.com>
2023-05-24 14:44:56 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<style>
+@font-face {
+  font-family: Inter;
+  src: url("../assets/fonts/Inter.woff2") format("woff2");
+  font-display: swap;
+}
+
+* {
+  margin: 0px;
+  padding: 0px;
+  scroll-behavior: smooth;
+  scrollbar-width: thin;
+  scrollbar-color: #444446 #171618;
+  box-sizing: border-box;
+  font-family: Inter !important;
+}
+</style>
