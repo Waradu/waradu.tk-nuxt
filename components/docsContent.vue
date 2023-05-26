b9a1c431fe5bf0eb9139803f577f6c39e2246481
Waradu <waradu@outlook.com>
2023-05-27 01:55:43 +0200

    commit

--- components/docsContent.vue
+++ components/docsContent.vue
@@ -17,46 +17,55 @@ export default {
   },
   methods: {
     calc(content: Object) {
-      var inner = ""
+      var inner = "";
 
       class Types {
-      static get img() {
-        return "normal";
-      }
+        static get img() {
+          return "normal";
+        }
 
-      static get stack() {
-        return "stack";
-      }
+        static get stack() {
+          return "stack";
+        }
 
-      static get space() {
-        return "stack";
-      }
+        static get space() {
+          return "stack";
+        }
 
-      static get text() {
-        return "stack";
-      }
+        static get text() {
+          return "stack";
+        }
 
-      static get list() {
-        return {
-          normal: "normal",
-          marked: "marked",
-        };
+        static get list() {
+          return {
+            normal: "normal",
+            marked: "marked",
+          };
+        }
       }
-    }
 
       class Templates {
         static img(src: String) {
-          return `<img src="~/assets/images/home.png" alt="image couldn't load" class="image" loading="lazy">`;
+          return `<img src="/_nuxt/assets/images/home.png" alt="image couldn't load" class="image" loading="lazy">`;
+        }
+
+        static stack(stack: Array<{}>) {
+          stack.forEach((stack_component) => {
+            if (stack_component.type == Types.text) {
+              inner += Templates.img(stack_component.src);
+            }
+          });
+          return `<img src="/_nuxt/assets/images/home.png" alt="image couldn't load" class="image" loading="lazy">`;
         }
       }
 
-      this.data.forEach(component => {
+      this.data.forEach((component) => {
         if (component.type == Types.img) {
-          inner += Templates.img(component.src)
+          inner += Templates.img(component.src);
         }
       });
 
-      return inner
+      return inner;
     },
     getAssetPath(src: String) {
       return `~/assets/images/${src}`;
