c82f3f1811bd1c9f0e1bbfbe0dd761990d42161f
Waradu <waradu@outlook.com>
2023-05-27 20:34:28 +0200

    commit

--- components/docsContent.vue
+++ components/docsContent.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="generated-content" v-html="page"></div>
+  <div class="generated-content">
+      <div v-for="item in generatedItems" :key="item" v-html="item"></div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -14,6 +16,14 @@ export default {
     page(): Object {
       return this.calc(this.data);
     },
+  },
+  data() {
+    return {
+      generatedItems: []
+    };
+  },
+  mounted() {
+    this.calc(this.data);
   },
   methods: {
     calc(content: Object) {
@@ -76,8 +86,9 @@ export default {
 
       this.data.forEach((component) => {
         if (component.type == Types.img) {
-          inner += Templates.img(component.src);
-        } else if (component.type == Types.space) {
+        const imgHtml = Templates.img(component.src);
+        this.generatedItems.push(imgHtml);
+      } else if (component.type == Types.space) {
           inner += Templates.space();
         } else if (component.type == Types.text) {
           inner += Templates.text(component.text);
