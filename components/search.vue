3097beb0e346b0f9dda67782e0f93334a4bbe88b
Waradu <waradu@outlook.com>
2023-05-30 13:15:10 +0200

    fixed search component: - show no results if nothing is searched - be able to run a command instead of opening a link

--- components/search.vue
+++ components/search.vue
@@ -1,32 +1,19 @@
 <template>
   <div class="searchContainer">
-    <div
-      class="search"
-      @keydown.enter="selectResult"
-      @keydown.up="navigateResults(-1)"
-      @keydown.down="navigateResults(1)"
-    >
-      <input
-        ref="searchInput"
-        type="text"
-        v-model="searchQuery"
-        placeholder="Search..."
-        @input="search"
-        @keydown.ctrl.75="toggleSearch"
-        @keydown.cmd.75="toggleSearch"
-      />
+    <div class="search" @keydown.enter="selectResult" @keydown.up="navigateResults(-1)" @keydown.down="navigateResults(1)">
+      <input ref="searchInput" type="text" v-model="searchQuery" placeholder="Search..." @input="search" @keydown.ctrl.75="toggleSearch" @keydown.cmd.75="toggleSearch" />
     </div>
     <div class="results" v-show="showResults">
       <ul>
-        <li
-          v-for="(result, index) in filteredResults"
-          :key="result.title"
-          :class="{ active: index === activeIndex }"
-        >
-          <a :href="result.link" @click="selectResult">
+        <li v-for="(result, index) in filteredResults" :key="result.title" :class="{ active: index === activeIndex }">
+          <a v-if="!result.command" :href="result.link" @click="selectResult">
             <h3>{{ result.title }}</h3>
             <p>{{ result.subTitle }}</p>
           </a>
+          <div v-else @click="executeCommand(result.command)">
+            <h3>{{ result.title }}</h3>
+            <p>{{ result.subTitle }}</p>
+          </div>
         </li>
       </ul>
     </div>
@@ -35,30 +22,33 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   data() {
     return {
-      searchQuery: "",
+      searchQuery: '',
       searchResults: [
         {
-          title: "TITLE 1",
-          subTitle: "SUBTITLE 1",
-          additinalSearchParameters: "help",
-          link: "LINK 1",
+          title: 'TITLE 1',
+          subTitle: 'SUBTITLE 1',
+          additionalSearchParameters: 'help',
+          link: 'LINK 1',
+          command: '',
         },
         {
-          title: "TITLE 2",
-          subTitle: "SUBTITLE 2",
-          additinalSearchParameters: "",
-          link: "LINK 2",
+          title: 'TITLE 2',
+          subTitle: 'SUBTITLE 2',
+          additionalSearchParameters: '',
+          link: 'LINK 2',
+          command: '',
         },
         {
-          title: "TITLE 3",
-          subTitle: "SUBTITLE 3",
-          additinalSearchParameters: "",
-          link: "LINK 3",
+          title: 'TITLE 3',
+          subTitle: 'SUBTITLE 3',
+          additionalSearchParameters: '',
+          link: '',
+          command: 'runCommand',
         },
       ],
       showResults: false,
@@ -68,39 +58,39 @@ export default defineComponent({
   computed: {
     filteredResults() {
       const query = this.searchQuery.toLowerCase();
-      if (query == "") {
-        this.showResults = false
+      if (query === '') {
+        this.showResults = false;
+      } else {
+        this.showResults = true;
       }
       return this.searchResults.filter(
-        (result) =>
+        result =>
           result.title.toLowerCase().includes(query) ||
           result.subTitle.toLowerCase().includes(query) ||
-          result.additinalSearchParameters.toLowerCase().includes(query)
+          result.additionalSearchParameters.toLowerCase().includes(query)
       );
     },
   },
   methods: {
     search() {
-      this.showResults = true;
       this.activeIndex = -1;
     },
     selectResult() {
-      if (
-        this.activeIndex !== -1 &&
-        this.activeIndex < this.filteredResults.length
-      ) {
+      if (this.activeIndex !== -1 && this.activeIndex < this.filteredResults.length) {
         const result = this.filteredResults[this.activeIndex];
-        window.location.href = result.link;
+        if (result.command !== '') {
+          this.executeCommand(result.command);
+        } else {
+          window.location.href = result.link;
+        }
       }
     },
     navigateResults(direction: number) {
-      this.activeIndex =
-        (this.activeIndex + direction + this.filteredResults.length) %
-        this.filteredResults.length;
+      this.activeIndex = (this.activeIndex + direction + this.filteredResults.length) % this.filteredResults.length;
     },
     toggleSearch(event: KeyboardEvent) {
       if (event.ctrlKey || event.metaKey) {
-        if (event.key === "k") {
+        if (event.key === 'k') {
           this.showResults = !this.showResults;
           if (this.showResults) {
             this.$nextTick(() => {
@@ -109,6 +99,9 @@ export default defineComponent({
           }
         }
       }
+    },
+    executeCommand(command: string) {
+      console.log('Executing command:', command);
     },
   },
 });
